5ea98a9ce493c11b0b7fda0f794b612e3d856ee1
Keneel <tkeneel@gmail.com>
2023-11-03 10:24:35 -0500

    Finish added structures to the webpage

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    const emailInput = document.querySelector("#email");
+    const messageDiv = document.querySelector(".message");
+  
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const email = emailInput.value;
+      if (email.trim() === "") {
+        messageDiv.textContent = "Please enter a valid email address.";
+      } else {
+        messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+      }
+    });
+  });
+  
